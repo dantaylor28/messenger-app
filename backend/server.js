efa64eeb1deaf8a53702538f86c1c7d33bb4bb66
Dan <dan.taylor1493@gmail.com>
2024-08-19 13:34:23 +0200

    Create .env & gitignore file, make port number dynamic

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
-
+const dotenv = require("dotenv");
 const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT || 5173;
 
 app.get("/", (req, res) => {
   // root route http://localhost:5173/
   res.send("Hello World!!");
 });
 
-app.listen(5173, () => console.log("Server running on port 5173"));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
