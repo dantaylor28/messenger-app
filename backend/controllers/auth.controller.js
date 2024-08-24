73a35fb0d8b5953a35908de30cf6216b7fcf040d
Dan <dan.taylor1493@gmail.com>
2024-08-24 12:22:18 +0200

    Chnage get requests to post for auth functions

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,11 +1,12 @@
 export const signup = (req, res) => {
+  res.send("Signup User");
   console.log("Signup user");
 };
 
 export const login = (req, res) => {
-    console.log("Login user");
-  };
+  console.log("Login user");
+};
 
-  export const logout = (req, res) => {
-    console.log("Logout user");
-  };
+export const logout = (req, res) => {
+  console.log("Logout user");
+};
