c512c24a23c0df456ad7f786ab9fc8b2c498c7a1
Dan <dan.taylor1493@gmail.com>
2024-09-01 10:57:40 +0200

    Check passwords match & user doesn't already exist

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,3 +1,5 @@
+import User from "../models/user.model.js";
+
 export const signup = async (req, res) => {
   try {
     const {
@@ -9,6 +11,18 @@ export const signup = async (req, res) => {
       gender,
       age,
     } = req.body;
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "Passwords do not match" });
+    }
+
+    const user = await User.findOne({ username });
+
+    if (user) {
+      return res.status(400).json({ error: "This username already exists" });
+    }
+
+    // Hash password here..
   } catch (error) {}
 };
 
