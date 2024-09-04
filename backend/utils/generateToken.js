829a0e75e5b4a131e4a57cda8d33bcd3bb912a8a
Dan <dan.taylor1493@gmail.com>
2024-09-04 13:42:18 +0200

    Set JWT token to a cookie

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -4,4 +4,12 @@ const generateToken = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "14d",
   });
+
+  res.cookie("jwtToken", token, {
+    maxAge: 14 * 24 * 60 * 60 * 1000, // 14d in milliseconds format
+    httpOnly: true, // Prevent XXS/cross-site scripting attacks
+    sameSite: "strict", // Prevent CSRF/cross-site request forgery attacks
+  });
 };
+
+export default generateToken;
