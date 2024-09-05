564e6ce8631aff3ecc436f0f007babaab00336cb
Dan <dan.taylor1493@gmail.com>
2024-09-05 14:53:26 +0200

    Add timestamps to the user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,39 +1,42 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  fullName: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    gender: {
+      type: String,
+      required: true,
+      enum: ["male", "female", "prefer not to say"],
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    profileImage: {
+      type: String,
+      default: "",
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 6,
-  },
-  gender: {
-    type: String,
-    required: true,
-    enum: ["male", "female", "prefer not to say"],
-  },
-  age: {
-    type: Number,
-    required: true,
-  },
-  profileImage: {
-    type: String,
-    default: "",
-  },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
