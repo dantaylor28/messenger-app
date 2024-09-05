65d48f51f609acb8b55522758549d0c8f3e72156
Dan <dan.taylor1493@gmail.com>
2024-09-05 16:30:14 +0200

    Create message.routes.js and add it to server.js file

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "./auth.routes.js";
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -10,6 +11,7 @@ dotenv.config();
 
 app.use(express.json()); // To parse the incoming requests with JSON payloads (from req.body)
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 // app.get("/", (req, res) => {
 //   // root route http://localhost:5173/
