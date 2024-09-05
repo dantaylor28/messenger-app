be9ca8055e26fe0e18aba74e6fea376efb6f4b78
Dan <dan.taylor1493@gmail.com>
2024-09-05 14:52:52 +0200

    Create message model

---
+++ backend/models/message.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    senderId: {
+        // Below type used as a reference to the user model.
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Message = mongoose.model("Message", messageSchema);
+
+export default Message;
