2155e5d59f43447041e8238185d529fbf6be4501
Dan <dan.taylor1493@gmail.com>
2024-09-09 15:01:47 +0200

    handle error if token is not found or decode it if it is

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -3,6 +3,15 @@ import jwt from "jsonwebtoken";
 const protectRoute = async (req, res, next) => {
   try {
     const token = req.cookies.jwt;
+    if (!token) {
+      return res
+        .status(401)
+        .json({ error: "Unauthorised access - No token has been provided" });
+    }
+    // Decode the cookie using verify method & JWT_SECRET
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    
   } catch (error) {
     console.log("Error in protectRoute middleware", error.message);
     res.status(500).json({ error: "Internal server error" });
