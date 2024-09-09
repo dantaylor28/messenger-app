85da8801c4aca426682b9e9f4fbb9faa776e25bd
Dan <dan.taylor1493@gmail.com>
2024-09-09 13:51:49 +0200

    Add a middleware, protectRoute, to ensure authentication is completed before a message can be sent

--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -2,7 +2,7 @@ import express from "express";
 import { sendMessage } from "../controllers/message.controller.js";
 
 const router = express.Router();
-
-router.post("/send/:id", sendMessage);
+// ProtectRoute is a middleware used to authenticate users ensuring they are allowed to send messages.
+router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
