5d2601ba1b423da821feb8ec2d05e13352366988
Dan <dan.taylor1493@gmail.com>
2024-09-09 16:39:30 +0200

    Handle any errors in protectRoute, return currently authenticated user and call next()

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
 
 const protectRoute = async (req, res, next) => {
   try {
@@ -11,9 +12,27 @@ const protectRoute = async (req, res, next) => {
     // Decode the cookie using verify method & JWT_SECRET
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    
+    if (!decoded) {
+      return res
+        .status(401)
+        .json({ error: "Unauthorised access - The token is invalid" });
+    }
+
+    const user = await User.findById(decoded.userId).select("-password");
+
+    if (!user) {
+      res.status(404).json({ error: "User not found" });
+    }
+
+    // The currently authenticated user
+    req.user = user;
+
+    // Calls sendMessage function in message.routes.js ie the next applicable code to run.
+    next();
   } catch (error) {
     console.log("Error in protectRoute middleware", error.message);
     res.status(500).json({ error: "Internal server error" });
   }
 };
+
+export default protectRoute;
--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { sendMessage } from "../controllers/message.controller.js";
+import protectRoute from "../middleware/protectRoute.js";
 
 const router = express.Router();
 // ProtectRoute is a middleware used to authenticate users ensuring they are allowed to send messages.
