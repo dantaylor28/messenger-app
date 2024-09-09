34fba92e6125fd0fd06365a783c45fcb7e55fd95
Dan <dan.taylor1493@gmail.com>
2024-09-09 14:32:16 +0200

    Gain access to cookies by using cookieParser and calling it as middleware in server.js

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -2,6 +2,7 @@ import jwt from "jsonwebtoken";
 
 const protectRoute = async (req, res, next) => {
   try {
+    const token = req.cookies.jwt;
   } catch (error) {
     console.log("Error in protectRoute middleware", error.message);
     res.status(500).json({ error: "Internal server error" });
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 5173;
 dotenv.config();
 
 app.use(express.json()); // To parse the incoming requests with JSON payloads (from req.body)
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
