599594d63bcc53faa8d9fde6644b4f6eabefbdd4
Dan <dan.taylor1493@gmail.com>
2024-09-09 13:50:50 +0200

    Attempt to get message, id and userId from the req

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,6 +1,8 @@
 export const sendMessage = async (req, res) => {
   try {
-    console.log("message sent successfully", req.params.id);
+    const { message } = req.body;
+    const { id } = req.params;
+    const senderId = req.userId;
   } catch (error) {
     console.log("Error in sendMessage controller", error.message);
     res.status(500).json({ error: "Internal server error" });
