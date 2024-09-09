5d2eb8f2d4dd303d1f90994ad6bf4ff361d2356c
Dan <dan.taylor1493@gmail.com>
2024-09-09 16:51:35 +0200

    Attempt to find chat which contains the 2 correct users

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,9 +1,18 @@
+import Chat from "../models/chat.model.js";
+
 export const sendMessage = async (req, res) => {
   try {
     const { message } = req.body;
-    const { id } = req.params;
+    // : changes name to receiverId
+    const { id: receiverId } = req.params;
     // req.user attained from protectRoute function
     const senderId = req.user._id;
+
+    // Find the chat which contains the 2 users.
+    // Using mongoDB syntax below to find chat which contains the sender and receiver IDs
+    let chat = await Chat.findOne({
+      participants: { $all: [senderId, receiverId] },
+    });
   } catch (error) {
     console.log("Error in sendMessage controller", error.message);
     res.status(500).json({ error: "Internal server error" });
