2d9d3b98bf03e6e1c7d9e21b18ea0cd81902667d
Dan <dan.taylor1493@gmail.com>
2024-09-10 07:32:08 +0200

    Correct jwt to jwtToken in protectRoute.js

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,10 @@ import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
+dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 5173;
-
-dotenv.config();
 
 app.use(express.json()); // To parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
