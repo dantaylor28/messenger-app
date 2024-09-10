2572e1bf086a498c12f656b644588388f01e7f9c
Dan <dan.taylor1493@gmail.com>
2024-09-10 17:22:03 +0200

    Fetch all users minus currentlyLoggedinUser and return an array of them

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,14 @@
+import User from "../models/user.model.js";
+
 export const getSidebarUsers = async (req, res) => {
   try {
     const loggedInUserId = req.user._id;
+
+    const fetchedUsers = await User.find({
+      _id: { $ne: loggedInUserId },
+    }).select("-password");
+
+    res.status(200).json(fetchedUsers);
   } catch (error) {
     console.log("Error in getSidebarUsers controller", error.message);
     res.status(500).json({ error: "Internal server error" });
