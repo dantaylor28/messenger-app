6d999e6198d14cf3bc05b43ac4db75bf762fa195
Dan <dan.taylor1493@gmail.com>
2024-09-17 10:33:45 +0200

    Create home, login and signup pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,11 @@
 import "./App.css";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <h1 className="text-red-300 font-extralight text-2xl">Hello world!!</h1>
+    <div className="h-screen flex items-center justify-center">
+      <Login />
+    </div>
   );
 }
 
