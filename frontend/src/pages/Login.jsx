6d0fd85f5d190725ee677213321e1f726ab59aca
Dan <dan.taylor1493@gmail.com>
2024-09-17 11:07:15 +0200

    Create skeleton login form

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="flex flex-col items-center justify-center">
+      <div className="bg-gray-200 p-10">
+        <h1>Login to your account</h1>
 
-export default Login
+        <form action="">
+          <div className="mt-5">
+            <label>
+              <div>Username:</div>
+            </label>
+            <input type="text" placeholder="Enter Username" />
+          </div>
+          <div className="mt-3">
+            <label>
+              <div>Password:</div>
+            </label>
+            <input type="text" placeholder="Enter Password" />
+          </div>
+
+          <div className="mt-3 text-sm">
+            Don't have an account?{" "}
+            <a href="#" className="text-red-500 underline">
+              Sign up here
+            </a>
+          </div>
+
+          <div className="text-center mt-5">
+            <button className="border border-black px-2">Login</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
