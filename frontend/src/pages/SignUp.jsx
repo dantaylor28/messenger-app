c264ced0aa1b1ca338d4893c97db92f5e17fe631
Dan <dan.taylor1493@gmail.com>
2024-09-22 19:20:46 +0200

    Add extra input to gender input field in form

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -43,14 +43,20 @@ const SignUp = () => {
 
           <div className="flex gap-4 mt-3">
             <div className="form-control">
-              <label className="flex gap-2">
+              <label className="flex gap-1">
                 <span>Male</span>
                 <input type="checkbox" />
               </label>
             </div>
             <div className="form-control">
-              <label className=" flex gap-2">
+              <label className=" flex gap-1">
                 <span>Female</span>
+                <input type="checkbox" />
+              </label>
+            </div>
+            <div className="form-control">
+              <label className="flex gap-1">
+                <span>Prefer not to say</span>
                 <input type="checkbox" />
               </label>
             </div>
