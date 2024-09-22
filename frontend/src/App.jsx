3c6293410fa6564956603abe428917fc07ef0240
Dan <dan.taylor1493@gmail.com>
2024-09-22 17:01:37 +0200

    Create skeleton sign up form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
     <div className="h-screen flex items-center justify-center">
-      <Login />
+      {/* <Login /> */}
+      <SignUp />
     </div>
   );
 }
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import React from "react";
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
-  )
-}
+    <div className="flex flex-col items-center justify-center">
+      <div className="bg-gray-200 p-10">
+        <h1>Sign up for an account</h1>
 
-export default SignUp
+        <form action="">
+          <div className="mt-5">
+            <label>
+              <div>Username:</div>
+            </label>
+            <input type="text" placeholder="Enter Username" />
+          </div>
+          <div className="mt-3">
+            <label>
+              <div>Email Address:</div>
+            </label>
+            <input type="text" placeholder="Enter Email" />
+          </div>
+          <div className="mt-3">
+            <label>
+              <div>Password:</div>
+            </label>
+            <input type="text" placeholder="Enter Password" />
+          </div>
+          <div className="mt-3">
+            <label>
+              <div>Confirm Password:</div>
+            </label>
+            <input type="text" placeholder="Confirm Password" />
+          </div>
+
+          {/* Enter age component */}
+          
+          <div className="flex gap-4">
+            <div className="form-control">
+              <label className="flex gap-2">
+                <span>Male</span>
+                <input type="checkbox" />
+              </label>
+            </div>
+            <div className="form-control">
+              <label className=" flex gap-2">
+                <span>Female</span>
+                <input type="checkbox" />
+              </label>
+            </div>
+          </div>
+
+          <div className="mt-3 text-sm">
+            Already have an account?{" "}
+            <a href="#" className="text-red-500 underline">
+              Sign in here
+            </a>
+          </div>
+
+          <div className="text-center mt-5">
+            <button className="border border-black px-2">Login</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
