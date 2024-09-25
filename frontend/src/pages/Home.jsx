10867671e04e058bc43acf37d3913cda2f4fbfb9
Dan <dan.taylor1493@gmail.com>
2024-09-25 14:45:52 +0200

    Create messageBox & display on home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Sidebar from '../components/Sidebar'
+import MessageBox from '../components/MessageBox'
 
 const Home = () => {
   return (
     <div className='flex h-[550px]'>
         <Sidebar />
+        <MessageBox />
     </div>
   )
 }
