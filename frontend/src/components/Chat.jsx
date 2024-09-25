a944ac1d537cd1cf509b7c2a5272d42733376a25
Dan <dan.taylor1493@gmail.com>
2024-09-25 10:59:44 +0200

    Create skeleton chat component and display them on Home.jsx

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const Chat = () => {
   return (
-    <div>My chats go here...</div>
-  )
-}
+    <>
+      <div className="flex gap-2 items-center cursor-pointer">
+        <div>
+          <div className="w-12 rounded-full">
+            <img
+              src="https://cdn0.iconfinder.com/data/icons/communication-line-10/24/account_profile_user_contact_person_avatar_placeholder-512.png"
+              alt="user avatar"
+            />
+          </div>
+        </div>
 
-export default Chat
+        <div className="flex flex-col flex-1">
+          <div className="flex gap-3 justify-between">
+            <p>Dan Taylor</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="h-1 bg-gray-200"/> 
+    </>
+  );
+};
+
+export default Chat;
--- frontend/src/components/Chats.jsx
+++ frontend/src/components/Chats.jsx
@@ -3,7 +3,12 @@ import Chat from './Chat'
 
 const Chats = () => {
   return (
-    <div>
+    <div className='flex flex-col py-4 overflow-auto'>
+        <Chat />
+        <Chat />
+        <Chat />
+        <Chat />
+        <Chat />
         <Chat />
     </div>
   )
