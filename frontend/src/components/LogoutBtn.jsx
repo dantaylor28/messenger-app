b99e6cac82546c9eee15fc7f8205f2afd7cef8d3
Dan <dan.taylor1493@gmail.com>
2024-09-25 11:26:21 +0200

    Create skeleton logout btn

---
+++ frontend/src/components/LogoutBtn.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LogoutBtn = () => {
+  return (
+    <div className='mt-auto'>
+        Logout
+    </div>
+  )
+}
+
+export default LogoutBtn
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import SearchBar from './SearchBar'
 import Chats from './Chats'
+import LogoutBtn from './LogoutBtn'
 
 const Sidebar = () => {
   return (
     <div className='border-r-4 border-gray-200 p-5 flex flex-col'>
         <SearchBar />
         <Chats />
+        <LogoutBtn />
     </div>
   )
 }
