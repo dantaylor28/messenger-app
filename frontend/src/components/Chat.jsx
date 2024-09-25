1c86098489ddea3a5fbba955494c129d4b3f5b58
Dan <dan.taylor1493@gmail.com>
2024-09-25 08:42:18 +0200

    Create Chats.jsx & Chat.jsx

---
+++ frontend/src/components/Chat.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Chat = () => {
+  return (
+    <div>My chats go here...</div>
+  )
+}
+
+export default Chat
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import SearchBar from './SearchBar'
+import Chats from './Chats'
 
 const Sidebar = () => {
   return (
     <div>
         <SearchBar />
+        <Chats />
     </div>
   )
 }
