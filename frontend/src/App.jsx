250ccd64cd6b9ef02a9e9fbda247e1b94a8e0913
Dan <dan.taylor1493@gmail.com>
2024-09-26 08:12:49 +0200

    Install react-router-dom and setup page routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -6,9 +7,11 @@ import SignUp from "./pages/SignUp";
 function App() {
   return (
     <div className="h-screen flex items-center justify-center">
-      {/* <Login /> */}
-      {/* <SignUp /> */}
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
     </div>
   );
 }
