41b278b573560f0dbefc025535daf831bb288833
Dan <dan.taylor1493@gmail.com>
2024-09-26 11:57:18 +0200

    Add logic to select gender in sign up form

--- frontend/src/components/GenderCheckbox.jsx
+++ frontend/src/components/GenderCheckbox.jsx
@@ -1,24 +1,48 @@
 import React from "react";
 
-const GenderCheckbox = () => {
+const GenderCheckbox = ({ onCheckboxChange, selectedGender }) => {
   return (
     <div className="flex gap-4 mt-3">
       <div className="form-control">
-        <label className="flex gap-1">
+        <label
+          className={`flex gap-1 ${
+            selectedGender === "male" ? "selected" : ""
+          }`}
+        >
           <span>Male</span>
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            checked={selectedGender === "male"}
+            onChange={() => onCheckboxChange("male")}
+          />
         </label>
       </div>
       <div className="form-control">
-        <label className=" flex gap-1">
+        <label
+          className={`flex gap-1 ${
+            selectedGender === "female" ? "selected" : ""
+          }`}
+        >
           <span>Female</span>
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            checked={selectedGender === "female"}
+            onChange={() => onCheckboxChange("female")}
+          />
         </label>
       </div>
       <div className="form-control">
-        <label className="flex gap-1">
+        <label
+          className={`flex gap-1 ${
+            selectedGender === "prefer not to say" ? "selected" : ""
+          }`}
+        >
           <span>Prefer not to say</span>
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            checked={selectedGender === "prefer not to say"}
+            onChange={() => onCheckboxChange("prefer not to say")}
+          />
         </label>
       </div>
     </div>
