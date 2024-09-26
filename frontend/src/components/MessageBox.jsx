8b3b1bf33d7f2fb015fdf461f76949687c14bc79
Dan <dan.taylor1493@gmail.com>
2024-09-26 07:54:53 +0200

    Create SelectChat component and render it depending on boolean value

--- frontend/src/components/MessageBox.jsx
+++ frontend/src/components/MessageBox.jsx
@@ -3,18 +3,34 @@ import Messages from "./Messages";
 import MessageBar from "./MessageBar";
 
 const MessageBox = () => {
+  const noChatSelected = false;
   return (
     <div className="flex flex-col min-w-[550px]">
-      <>
-        <div className="bg-gray-200 px-2 py-4 mb-5">
+      {noChatSelected ? (
+        <SelectChat />
+      ) : (
+        <>
+          <div className="bg-gray-200 px-2 py-4 mb-5">
             <span>To:</span> <span>Sabina Boije</span>
-        </div>
+          </div>
 
-        <Messages />
-        <MessageBar />
-      </>
+          <Messages />
+          <MessageBar />
+        </>
+      )}
     </div>
   );
 };
 
 export default MessageBox;
+
+const SelectChat = () => {
+  return (
+    <div className="flex items-center justify-center w-full h-full">
+      <div className="">
+        <p>Hi Dan Taylor👋</p>
+        <p>Choose a chat to begin!</p>
+      </div>
+    </div>
+  );
+};
