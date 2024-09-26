eff5fcf7f981cdb97027addf1eea9863ac6dbdf2
Dan <dan.taylor1493@gmail.com>
2024-09-26 11:15:25 +0200

    Add value & onChange handlers to signup form

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -9,43 +9,71 @@ const SignUp = () => {
     password: "",
     confirmPassword: "",
     gender: "",
-    age: ""
-  })
+    age: "",
+  });
   return (
     <div className="flex flex-col items-center justify-center">
       <div className="bg-gray-200 p-10">
         <h1>Sign up for an account</h1>
 
         <form action="">
-        <div className="mt-5">
+          <div className="mt-5">
             <label>
               <div>Full Name:</div>
             </label>
-            <input type="text" placeholder="Enter Full Name" required />
+            <input
+              type="text"
+              placeholder="Enter Full Name"
+              required
+              value={userInputs.fullName}
+              onChange={(e) =>
+                setUserInputs({ ...userInputs, fullName: e.target.value })
+              }
+            />
           </div>
           <div className="mt-5">
             <label>
               <div>Username:</div>
             </label>
-            <input type="text" placeholder="Enter Username" required />
+            <input
+              type="text"
+              placeholder="Enter Username"
+              required
+              value={userInputs.username}
+              onChange={(e) =>
+                setUserInputs({ ...userInputs, username: e.target.value })
+              }
+            />
           </div>
           <div className="mt-3">
             <label>
               <div>Email Address:</div>
             </label>
-            <input type="email" placeholder="Enter Email" required />
+            <input type="email" placeholder="Enter Email" required
+            value={userInputs.email}
+            onChange={(e) =>
+              setUserInputs({ ...userInputs, email: e.target.value })
+            } />
           </div>
           <div className="mt-3">
             <label>
               <div>Password:</div>
             </label>
-            <input type="password" placeholder="Enter Password" required />
+            <input type="password" placeholder="Enter Password" required
+            value={userInputs.password}
+            onChange={(e) =>
+              setUserInputs({ ...userInputs, password: e.target.value })
+            } />
           </div>
           <div className="mt-3">
             <label>
               <div>Confirm Password:</div>
             </label>
-            <input type="password" placeholder="Confirm Password" required />
+            <input type="password" placeholder="Confirm Password" required
+            value={userInputs.confirmPassword}
+            onChange={(e) =>
+              setUserInputs({ ...userInputs, confirmPassword: e.target.value })
+            } />
           </div>
 
           <div className="flex mt-3">
@@ -53,7 +81,11 @@ const SignUp = () => {
               <label>
                 <div>Enter Age:</div>
               </label>
-              <input type="number" min="10" max="120" required />
+              <input type="number" min="10" max="120" required 
+              value={userInputs.age}
+              onChange={(e) =>
+                setUserInputs({ ...userInputs, age: e.target.value })
+              }/>
             </div>
           </div>
 
