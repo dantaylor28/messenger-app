88fc28a7519546992e5760a9546af4e20e500ab7
Dan <dan.taylor1493@gmail.com>
2024-09-29 07:50:36 +0200

    Declare handleSignupErrors function & install react-hot-toast

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
+import Toaster from "react-hot-toast";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
+      <Toaster />
     </div>
   );
 }
--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,11 +1,60 @@
 import React, { useState } from "react";
+import toast from "react-hot-toast";
 
 const useSignup = () => {
   const [sendingData, setSendingData] = useState(false);
 };
 
-const signupUser = async ({fullName, email, username, password, confirmPassword, age, gender}) => {
-    
-}
+const signupUser = async ({
+  fullName,
+  email,
+  username,
+  password,
+  confirmPassword,
+  age,
+  gender,
+}) => {
+  const success = handleSignupErrors({
+    fullName,
+    email,
+    username,
+    password,
+    confirmPassword,
+    age,
+    gender,
+  });
+  if (!success) return;
+};
 
 export default useSignup;
+
+function handleSignupErrors({
+  fullName,
+  email,
+  username,
+  password,
+  confirmPassword,
+  age,
+  gender,
+}) {
+  if (
+    !fullName ||
+    !email ||
+    !username ||
+    !password ||
+    !confirmPassword ||
+    !age ||
+    !gender
+  ) {
+    toast.error("Please fill in all fields");
+    return false;
+  }
+  if (password !== confirmPassword) {
+    toast.error("Your passwords do not match");
+    return false;
+  }
+  if (password.length < 6) {
+    toast.error("Passwords must be at least 6 characters");
+    return false;
+  }
+}
