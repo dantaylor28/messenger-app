35d7a4806cba5a86944f1010f83c604b55ebc6e6
Dan <dan.taylor1493@gmail.com>
2024-10-03 06:45:49 +0200

    Add port & proxy to vite.config file

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -27,7 +27,7 @@ const useSignup = () => {
     setSendingData(true);
 
     try {
-      const res = await fetch("http://localhost:5173/api/auth/signup", {
+      const res = await fetch("/api/auth/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
