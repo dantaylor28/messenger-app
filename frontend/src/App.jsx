1b36113e34df0254af650134f968f7490916c0e6
Dan <dan.taylor1493@gmail.com>
2024-10-04 11:35:47 +0200

    Protect routes in App.jsx by redirecting user depending if authenticatedUser exists

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,22 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes, redirect } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import { Toaster } from "react-hot-toast";
+import { useAuthContext } from "./context/AuthContext";
 
 function App() {
+  const { authenticatedUser } = useAuthContext();
   return (
     <div className="h-screen flex items-center justify-center">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
+        <Route path="/" element={authenticatedUser ? <Home /> : <Navigate to="/login" />} />
+        <Route path="/login" element={authenticatedUser ? <Navigate to="/" /> : <Login />} />
+        <Route
+          path="/signup"
+          element={authenticatedUser ? <Navigate to="/" /> : <SignUp />}
+        />
       </Routes>
       <Toaster />
     </div>
