3764a5d64a7551269af3aade9c52799ad04e034a
Dan <dan.taylor1493@gmail.com>
2024-10-07 07:43:21 +0200

    Create useLogout hook and add functionality

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const useLogout = () => {
-  return (
-    <div>useLogout</div>
-  )
-}
+  const [sendingData, setSendingData] = useState(false);
+  const { setAuthenticatedUser } = useAuthContext();
 
-export default useLogout
+  const logoutUser = async () => {
+    setSendingData(true);
+    try {
+      const res = await fetch("/api/auth/logout", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+      });
+      const data = await res.json();
+      if (data.error) {
+        throw new Error(data.error);
+      }
+
+      localStorage.removeItem("authenticatedUser");
+      setAuthenticatedUser(null);
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setSendingData(false);
+    }
+  };
+
+  return { sendingData, logoutUser };
+};
+
+export default useLogout;
