dca5ffefaae3ecd4bb25c9fa2dde83b0126a5c58
Dan <dan.taylor1493@gmail.com>
2024-10-16 14:53:25 +0200

    Display "loading.." if sendingData is true for the logout btn

--- frontend/src/components/LogoutBtn.jsx
+++ frontend/src/components/LogoutBtn.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import useLogout from '../hooks/useLogout'
+import React from "react";
+import useLogout from "../hooks/useLogout";
 
 const LogoutBtn = () => {
-  const {sendingData, logoutUser} = useLogout()
+  const { sendingData, logoutUser } = useLogout();
   return (
-    <div className='mt-auto cursor-pointer' onClick={logoutUser}>
+    <div className="mt-auto">
+      {!sendingData ? (<div className="cursor-pointer" onClick={logoutUser}>
         Logout
+      </div>) : (
+        <span>
+          Loading..
+        </span>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default LogoutBtn
+export default LogoutBtn;
