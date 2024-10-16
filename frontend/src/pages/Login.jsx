f32f4924c532f0e217d4aff38fd83423cba4dbad
Dan <dan.taylor1493@gmail.com>
2024-10-16 15:09:33 +0200

    Add onclicks and value fields to login inputs

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,24 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <div className="flex flex-col items-center justify-center">
       <div className="bg-gray-200 p-10">
         <h1>Login to your account</h1>
 
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <div className="mt-5">
             <label>
               <div>Username:</div>
             </label>
-            <input type="text" placeholder="Enter Username" required />
+            <input type="text" placeholder="Enter Username" required value={username} onChange={(e) => setUsername(e.target.value)} />
           </div>
           <div className="mt-3">
             <label>
               <div>Password:</div>
             </label>
-            <input type="text" placeholder="Enter Password" required />
+            <input type="text" placeholder="Enter Password" required value={password} onChange={(e) => setPassword(e.target.value)} />
           </div>
 
           <div className="mt-3 text-sm">
