cac01681b76f5f9d20b694665638a4b1b20500f8
Dan <dan.taylor1493@gmail.com>
2024-10-16 15:35:37 +0200

    Add complete logic to loginUser function

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -1,16 +1,34 @@
 import React, { useState } from "react";
+import { useAuthContext } from "../context/AuthContext";
 
 const useLogin = () => {
   const [sendingData, setSendingData] = useState(false);
+  const { setAuthenticatedUser } = useAuthContext();
 
   const loginUser = async (username, password) => {
     setSendingData(true);
     try {
+      const res = await fetch("/api/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, password }),
+      });
+
+      const data = await res.json();
+      if (data.error) {
+        throw new Error(data.error);
+      }
+
+      localStorage.setItem("authenticatedUser", JSON.stringify(data));
+      setAuthenticatedUser(data);
     } catch (error) {
+      toast.error(error.message);
     } finally {
       setSendingData(false);
     }
   };
+
+  return { sendingData, loginUser };
 };
 
 export default useLogin;
