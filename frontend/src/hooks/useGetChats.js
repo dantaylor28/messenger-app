9c33643ee4d135fd9a3d0a7450bb08395228a7af
Dan <dan.taylor1493@gmail.com>
2024-10-17 14:37:24 +0200

    Add useEffect to useGetChats hook

--- frontend/src/hooks/useGetChats.js
+++ frontend/src/hooks/useGetChats.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const useGetChats = () => {
-  return (
-    <div>useGetChats</div>
-  )
-}
+  const [sendingData, setSendingData] = useState(false);
+  const [chats, setChats] = useState([]);
 
-export default useGetChats
+  useEffect(() => {
+    const getChats = async () => {
+      setSendingData(true);
+      try {
+        const res = await fetch("/api/users");
+        const data = await res.json();
+        if (data.error) {
+          throw new Error(data.error);
+        }
+        setChats(data);
+      } catch (error) {
+        toast.error(error.message);
+      } finally {
+        setSendingData(false);
+      }
+    };
+    getChats();
+  }, []);
+  return { sendingData, chats };
+};
+
+export default useGetChats;
