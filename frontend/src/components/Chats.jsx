61e6a99832c1ba5369b0b15ee8cc1558380169aa
Dan <dan.taylor1493@gmail.com>
2024-10-17 14:48:46 +0200

    Map over chats to display them 1 by 1

--- frontend/src/components/Chats.jsx
+++ frontend/src/components/Chats.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
-import Chat from './Chat'
+import React from "react";
+import Chat from "./Chat";
+import useGetChats from "../hooks/useGetChats";
 
 const Chats = () => {
+  const { sendingData, chats } = useGetChats();
+  console.log(chats);
   return (
-    <div className='flex flex-col py-4 overflow-auto'>
-        <Chat />
-        <Chat />
-        <Chat />
-        <Chat />
-        <Chat />
-        <Chat />
+    <div className="flex flex-col py-4 overflow-auto">
+      {chats.map((chat) => {
+        <Chat key={chat._id} chat={chat}/>
+      })}
+      {sendingData ? <span>Loading..</span> : null}
     </div>
-  )
-}
+  );
+};
 
-export default Chats
+export default Chats;
