d1a835a956abbf0b7374df298fac8ab87c0dfbeb
Dan <dan.taylor1493@gmail.com>
2024-10-22 13:54:38 +0200

    Call sendMessage function in messageBar component & update state values

--- frontend/src/components/MessageBar.jsx
+++ frontend/src/components/MessageBar.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { useState } from "react";
+import useSendMessage from "../hooks/useSendMessage";
 
 const MessageBar = () => {
+  const [message, setMessage] = useState("");
+  const { sendingData, sendMessage } = useSendMessage();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!message) return;
+    await sendMessage(message);
+    setMessage("");
+  };
   return (
-    <form>
-        <div className='w-full relative'>
-            <input type="text" placeholder='Enter message' className='w-full border border-black' />
-            <button type='submit' className='absolute inset-y-0 end-0 items-center pe-3'>Send</button>
-        </div>
+    <form onSubmit={handleSubmit}>
+      <div className="w-full relative">
+        <input
+          type="text"
+          placeholder="Enter message"
+          className="w-full border border-black"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button
+          type="submit"
+          className="absolute inset-y-0 end-0 items-center pe-3"
+        >
+          {sendingData ? <span>Loading..</span> : "Send"}
+        </button>
+      </div>
     </form>
-  )
-}
+  );
+};
 
-export default MessageBar
+export default MessageBar;
