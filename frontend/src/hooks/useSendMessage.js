313c9646820c8064c306dc6c117eeaed994b6636
Dan <dan.taylor1493@gmail.com>
2024-10-22 13:17:56 +0200

    Define useSendMessage hook

--- frontend/src/hooks/useSendMessage.js
+++ frontend/src/hooks/useSendMessage.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import { useState } from "react";
+import { useChatContext } from "../context/ChatContext";
 
 const useSendMessage = () => {
-  return (
-    <div>useSendMessage</div>
-  )
-}
+  const [sendingData, setSendingData] = useState(false);
+  const { messages, setMessages, selectedChat } = useChatContext();
 
-export default useSendMessage
+  const sendMessage = async (message) => {
+    setSendingData(true);
+    try {
+      const res = await fetch(`/api/messages/send/${selectedChat._id}`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ message }),
+      });
+      const data = await res.json();
+      if (data.error) {
+        throw new Error(data.error);
+      }
+      setMessages([...messages, data]);
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setSendingData(false);
+    }
+  };
+  return { sendMessage, sendingData };
+};
+
+export default useSendMessage;
