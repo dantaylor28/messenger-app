591553b9195671c643e51407f4267b25548c9016
Dan <dan.taylor1493@gmail.com>
2024-10-23 07:38:07 +0200

    Define useGetMessages hook

--- frontend/src/hooks/useGetMessages.js
+++ frontend/src/hooks/useGetMessages.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useChatContext } from "../context/ChatContext";
 
 const useGetMessages = () => {
-  return (
-    <div>useGetMessages</div>
-  )
-}
+  const [sendingData, setSendingData] = useState(false);
+  const { messages, setMessages, selectedChat } = useChatContext();
 
-export default useGetMessages
+  useEffect(() => {
+    const getMessages = async () => {
+      setSendingData(true);
+      try {
+        const res = await fetch(`/api/messages/${selectedChat._id}`);
+        const data = await res.json();
+        if (data.error) {
+          throw new Error(data.error);
+        }
+        setMessages(data);
+      } catch (error) {
+        toast.error(error.message);
+      } finally {
+        setSendingData(false);
+      }
+    };
+    if (selectedChat?._id) getMessages();
+  }, [selectedChat?._id, setMessages]);
+
+  return { messages, sendingData };
+};
+
+export default useGetMessages;
