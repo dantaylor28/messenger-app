f80b9701b9f1ad0159deed48941caa019a8f011c
Dan <dan.taylor1493@gmail.com>
2024-10-23 07:43:50 +0200

    Console log messages to ensure functionality is working as expected

--- frontend/src/components/Messages.jsx
+++ frontend/src/components/Messages.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import Message from "./Message";
+import useGetMessages from "../hooks/useGetMessages";
 
 const Messages = () => {
+  const { messages, sendingData } = useGetMessages();
+  console.log(messages);
   return (
     <div className="flex-1 overflow-auto px-4">
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
     </div>
   );
 };
