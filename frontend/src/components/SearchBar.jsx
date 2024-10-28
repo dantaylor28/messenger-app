ec29265d4c30fc4311449fd7a6720219b7b65b40
Dan <dan.taylor1493@gmail.com>
2024-10-28 07:29:24 +0100

    Add search user functionality to searchBar

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,12 +1,43 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useChatContext } from "../context/ChatContext";
+import useGetChats from "../hooks/useGetChats";
+import toast from "react-hot-toast"
 
 const SearchBar = () => {
-  return (
-    <form className='flex items-center gap-2'>
-        <input type="text" placeholder='Search for a user' className='border border-black'/>
-        <button type='submit'>Search</button>
-    </form>
-  )
-}
+  const [search, setSearch] = useState("");
+  const { setSelectedChat } = useChatContext();
+  const { chats } = useGetChats();
 
-export default SearchBar
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!search) return;
+    if (search.length < 3) {
+      return toast.error("Search must contain 3 or more characters");
+    }
+    const chat = chats.find((chat) =>
+      chat.fullName.toLowerCase().includes(search.toLowerCase())
+    );
+
+    if (chat) {
+      setSelectedChat(chat);
+      setSearch("");
+    } else {
+      toast.error("No such user can be found");
+    }
+  };
+
+  return (
+    <form className="flex items-center gap-2" onSubmit={handleSubmit}>
+      <input
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        type="text"
+        placeholder="Search for a user"
+        className="border border-black"
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+};
+
+export default SearchBar;
