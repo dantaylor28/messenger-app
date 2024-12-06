70657c444d6902d836017feb476f18496f189ea0
Dan <dan.taylor1493@gmail.com>
2024-12-06 08:52:49 +0100

    Add final styles to message bar

--- frontend/src/components/MessageBar.jsx
+++ frontend/src/components/MessageBar.jsx
@@ -17,7 +17,7 @@ const MessageBar = () => {
         <input
           type="text"
           placeholder="Enter message"
-          className="border-y border-r border-black/20 w-full h-10 rounded-sm text-md pl-2 shadow-sm cursor-text"
+          className="border-y border-r border-black/20 w-full h-12 rounded-sm text-md pl-2 shadow-sm cursor-text"
           value={message}
           onChange={(e) => setMessage(e.target.value)}
         />
@@ -25,7 +25,7 @@ const MessageBar = () => {
           type="submit"
           className="absolute inset-y-0 end-0 items-center pe-3"
         >
-          {sendingData ? <span>Loading..</span> : <IoSend className="h-[22px] w-[22px] text-cyan-600 hover:text-cyan-700 hover:translate-x-0.5 transition"/>}
+          {sendingData ? <span>Loading..</span> : <IoSend className="h-[22px] w-[22px] text-cyan-600 hover:text-cyan-700 hover:translate-x-0.5 transition hover:cursor-pointer"/>}
         </button>
       </div>
     </form>
