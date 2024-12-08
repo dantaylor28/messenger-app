6f7028f0057f7eca5a2d64301538c5f4756bad05
Dan <dan.taylor1493@gmail.com>
2024-12-08 20:11:08 +0100

    Correct layout and start styling of message components

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -12,39 +12,27 @@ const Message = ({ message }) => {
     : selectedChat?.profileImage;
   const chatBubbleLayout = sentFromMe ? "justify-end" : "items-start";
   const chatBubbleBg = sentFromMe ? "bg-green-500" : "";
+  const timeLayout = sentFromMe ? "justify-end mr-14" : "items-start ml-14";
   const formattedTime = formatTime(message.createdAt);
 
-  const chatBubble = <><div className="border border-black/15 p-2 rounded-xl max-w-[35%]">{message.message}</div></>
+  const chatBubble = (
+    <>
+      <div className={`border border-black/15 p-2 rounded-xl max-w-[35%] ${chatBubbleBg}`}>
+        {message.message}
+      </div>
+    </>
+  );
 
   return (
-    // <div className={`mb-3 flex ${chatBubbleLayout}`}>
-    //   <div className="flex items-center space-x-4">
-    //     <div>
-    //       <div className={`w-10 rounded-full ${profileImageLayout}`}>
-    //         <img src={profileImage} alt="chat user avatar" />
-    //       </div>
-    //     </div>
-    //     <div className={`border border-black p-2 rounded-lg ${chatBubbleBg}`}>
-    //       {message.message}
-    //     </div>
-    //   </div>
-    //   <div className="text-xs">{formattedTime}</div>
-    // </div>
-
-    <div className={`mb-3 flex space-x-3 ${chatBubbleLayout}`}>
-      {sentFromMe ? (
-        chatBubble
-      ) : (
-        ""
-      )}
-      <div className="h-10 w-10 rounded-full border flex">
-        <img src={profileImage} alt="chat user avatar" />
+    <div className="flex-col mb-3">
+      <div className={`flex space-x-3 ${chatBubbleLayout}`}>
+        {sentFromMe ? chatBubble : ""}
+        <div className="h-10 w-10 rounded-full border flex">
+          <img src={profileImage} alt="chat user avatar" />
+        </div>
+        {sentFromMe ? "" : chatBubble}
       </div>
-      {sentFromMe ? (
-        ""
-      ) : (
-        chatBubble
-      )}
+      <div className={`flex text-xs mt-1 ${timeLayout}`}>{formattedTime}</div>
     </div>
   );
 };
