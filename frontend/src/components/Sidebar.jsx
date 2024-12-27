946e6076ca2ccc0587743e9a6c5b71a4df890da4
Dan <dan.taylor1493@gmail.com>
2024-12-27 12:45:49 +0100

    Adjust messageBox & sidebar at different breakpoints and ensure working as expected

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -5,7 +5,7 @@ import LogoutBtn from './LogoutBtn'
 
 const Sidebar = () => {
   return (
-    <div className='hidden md:flex flex-col min-w-[400px] border border-black/10 rounded-sm p-5'>
+    <div className='hidden md:flex flex-col md:min-w-[300px] lg:min-w-[350px] xl:min-w-[400px] border border-black/10 rounded-sm p-5'>
         <SearchBar />
         <Chats />
         <LogoutBtn />
