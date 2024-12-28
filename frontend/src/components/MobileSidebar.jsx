e3aa8e5662fcb9f4bdfa5d83159311b3aebffcb1
Dan <dan.taylor1493@gmail.com>
2024-12-28 19:14:01 +0100

    Add react icons for the open & close btns

--- frontend/src/components/MobileSidebar.jsx
+++ frontend/src/components/MobileSidebar.jsx
@@ -1,13 +1,18 @@
-import React, { useState } from "react";
+import { useState } from "react";
+import { RxHamburgerMenu, RxCross1 } from "react-icons/rx";
 
 const MobileSidebar = () => {
   const [expanded, setExpanded] = useState(false);
   return (
     <>
     {expanded ? (
-        <button className="md:hidden flex fixed top-6 left-10 z-20" onClick={() => setExpanded(!expanded)}>close</button>
+        <button className="md:hidden flex fixed top-6 left-10 z-20 text-2xl text-white" onClick={() => setExpanded(!expanded)}>
+            <RxCross1 />
+        </button>
     ) : (
-        <button className="md:hidden flex fixed top-6 left-10 z-20" onClick={() => setExpanded(!expanded)}>open</button>
+        <button className="md:hidden flex fixed top-6 left-10 z-20 text-2xl" onClick={() => setExpanded(!expanded)}>
+            <RxHamburgerMenu />
+        </button>
     )}
       <div className={`md:hidden top-0 left-0 w-[50vw] p-10 fixed h-full bg-cyan-600 text-white z-10 ease-in-out duration-700 ${expanded ? "-translate-x-0" : "-translate-x-full"}`}>
         <h2>MobileSidebar</h2>
