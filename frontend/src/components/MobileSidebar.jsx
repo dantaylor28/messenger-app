50a1dba1fa15f22f050ccaba4b63f292b3392edd
Dan <dan.taylor1493@gmail.com>
2024-12-28 19:03:24 +0100

    Add smooth transition to mobileSidebar

--- frontend/src/components/MobileSidebar.jsx
+++ frontend/src/components/MobileSidebar.jsx
@@ -9,7 +9,7 @@ const MobileSidebar = () => {
     ) : (
         <button className="md:hidden flex fixed top-6 left-10 z-20" onClick={() => setExpanded(!expanded)}>open</button>
     )}
-      <div className={`md:hidden top-0 left-0 w-[50vw] p-10 fixed h-full bg-cyan-600 text-white z-10 ${expanded ? "-translate-x-0" : "-translate-x-full"}`}>
+      <div className={`md:hidden top-0 left-0 w-[50vw] p-10 fixed h-full bg-cyan-600 text-white z-10 ease-in-out duration-700 ${expanded ? "-translate-x-0" : "-translate-x-full"}`}>
         <h2>MobileSidebar</h2>
       </div>
     </>
