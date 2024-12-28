28077b42ef2e3885c6be76da6bf8e442438cde76
Dan <dan.taylor1493@gmail.com>
2024-12-28 18:42:20 +0100

    Add functionality to open & close sidebar component

--- frontend/src/components/MobileSidebar.jsx
+++ frontend/src/components/MobileSidebar.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const MobileSidebar = () => {
+  const [expanded, setExpanded] = useState(false);
   return (
-    <div className='md:hidden top-0 left-0 w-[50vw] p-10 pl-20 fixed h-full bg-cyan-600 text-white z-10'>
+    <>
+    {expanded ? (
+        <button className="md:hidden flex fixed top-6 left-10 z-20" onClick={() => setExpanded(!expanded)}>close</button>
+    ) : (
+        <button className="md:hidden flex fixed top-6 left-10 z-20" onClick={() => setExpanded(!expanded)}>open</button>
+    )}
+      <div className={`md:hidden top-0 left-0 w-[50vw] p-10 fixed h-full bg-cyan-600 text-white z-10 ${expanded ? "-translate-x-0" : "-translate-x-full"}`}>
         <h2>MobileSidebar</h2>
-    </div>
-  )
-}
+      </div>
+    </>
+  );
+};
 
-export default MobileSidebar
+export default MobileSidebar;
