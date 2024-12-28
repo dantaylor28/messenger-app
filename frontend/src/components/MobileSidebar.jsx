3638bca2069f22f87e6ae98bf4478e90bc717d21
Dan <dan.taylor1493@gmail.com>
2024-12-28 07:06:02 +0100

    Create and render an empty sidebar for mobile screens

--- frontend/src/components/MobileSidebar.jsx
+++ frontend/src/components/MobileSidebar.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const MobileSidebar = () => {
   return (
-    <div>MobileSidebar</div>
+    <div className='md:hidden top-0 left-0 w-[50vw] p-10 pl-20 fixed h-full bg-cyan-600 text-white z-10'>
+        <h2>MobileSidebar</h2>
+    </div>
   )
 }
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Sidebar from '../components/Sidebar'
 import MessageBox from '../components/MessageBox'
+import MobileSidebar from '../components/MobileSidebar'
 
 const Home = () => {
   return (
     <div className='flex'>
         <Sidebar />
+        <MobileSidebar />
         <MessageBox />
     </div>
   )
