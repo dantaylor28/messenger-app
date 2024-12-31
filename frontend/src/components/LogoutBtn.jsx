506cee66acfee55db20d28d8d93734adbf2d65a2
Dan <dan.taylor1493@gmail.com>
2024-12-31 06:23:40 +0100

    Change logoutBtn color on md screens in mobileSidebar

--- frontend/src/components/LogoutBtn.jsx
+++ frontend/src/components/LogoutBtn.jsx
@@ -7,7 +7,7 @@ const LogoutBtn = () => {
   return (
     <div className="mt-auto">
       {!sendingData ? (<div className="cursor-pointer" onClick={logoutUser}>
-        <CiLogout className="h-8 w-8 text-white/80 sm:text-cyan-600 hover:text-white sm:hover:text-cyan-700 hover:scale-110 transition"/>
+        <CiLogout className="h-8 w-8 text-white/80 md:text-cyan-600 hover:text-white md:hover:text-cyan-700 hover:scale-110 transition"/>
       </div>) : (
         <div className="animate-spin h-5 w-5 rounded-full border-b-2 border-white sm:border-cyan-800"></div>
       )}
