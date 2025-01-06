3f6dd7b3dc348bb587decb8c44b91b986dbd1651
Dan <dan.taylor1493@gmail.com>
2025-01-06 21:21:42 +0100

    Finish implementing socket.io to see onlineUsers

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -12,12 +12,22 @@ const io = new Server(server, {
   },
 });
 
+const userSocketMap = {}; // {userId:socketId}
+
 io.on("connection", (socket) => {
   console.log("User connected", socket.id);
+
+  const userId = socket.handshake.query.userId;
+  if (userId != "undefined") userSocketMap[userId] = socket.id;
+
+  // io.emit() is used to send events to all connected clients
+  io.emit("onlineUsers", Object.keys(userSocketMap));
 
   // socket.on() used to listen to the events, on both client and server side
   socket.on("disconnect", () => {
     console.log("User disconnected", socket.id);
+    delete userSocketMap[userId];
+    io.emit("onlineUsers", Object.keys(userSocketMap));
   });
 });
 
--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -1,8 +1,12 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useState, useContext } from "react";
 import { useAuthContext } from "./AuthContext";
 import io from "socket.io-client";
 
-export const SocketContext = createContext();
+const SocketContext = createContext();
+
+export const useSocketContext = () => {
+  return useContext(SocketContext);
+};
 
 export const SocketContextProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
@@ -11,9 +15,18 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authenticatedUser) {
-      const socket = io("http://localhost:5173");
+      const socket = io("http://localhost:5173", {
+        query: {
+          userId: authenticatedUser._id,
+        },
+      });
 
       setSocket(socket);
+
+      // socket.on() used to listen to the events, on both client and server side
+      socket.on("onlineUsers", (users) => {
+        setOnlineUsers(users);
+      });
 
       return () => socket.close();
     } else {
