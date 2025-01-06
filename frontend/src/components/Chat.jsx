25cd18ee5382848de61494cbfaedcfc0114229dc
Dan <dan.taylor1493@gmail.com>
2025-01-06 21:24:40 +0100

    use SocketContext to display if users are on/offline in Chat.jsx file

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -1,32 +1,46 @@
 import React from "react";
 import { useChatContext } from "../context/ChatContext";
+import { useSocketContext } from "../context/SocketContext";
 
 const Chat = ({ chat, onClick, lastIndex }) => {
   const { selectedChat, setSelectedChat } = useChatContext();
 
   const isSelected = selectedChat?._id === chat._id;
+  const { onlineUsers } = useSocketContext();
+  const isOnline = onlineUsers.includes(chat._id);
   return (
     <>
       <div
         className={`flex gap-4 py-2.5 px-2 rounded-sm items-center cursor-pointer hover:bg-cyan-700 md:hover:bg-gray-200 ${
           isSelected ? "bg-cyan-700 md:bg-gray-200" : ""
         }`}
-        onClick={() => { setSelectedChat(chat); onClick();}}
+        onClick={() => {
+          setSelectedChat(chat);
+          onClick();
+        }}
       >
         <div className="py-1.5">
           <div className="w-12 md:w-14 rounded-full border border-black/10 relative">
             <img src={chat.profileImage} alt="user avatar" />
-            <span className="h-3 w-3 rounded-full bg-green-500 absolute top-[-1px] right-[3.5px]"></span>
+            <span
+              className={`h-3 w-3 rounded-full ${
+                isOnline ? "bg-green-500" : "bg-gray-300"
+              } absolute top-[-1px] right-[3.5px]`}
+            ></span>
           </div>
         </div>
 
         <div className="flex flex-col flex-1">
           <div>
-            <p className="tracking-wide capitalize font-light">{chat.fullName}</p>
+            <p className="tracking-wide capitalize font-light">
+              {chat.fullName}
+            </p>
           </div>
         </div>
       </div>
-      {!lastIndex && <hr className="bg-white opacity-20 md:bg-none md:opacity-100" />}
+      {!lastIndex && (
+        <hr className="bg-white opacity-20 md:bg-none md:opacity-100" />
+      )}
     </>
   );
 };
