2cffe3ef56ba5108ecd856f3a1c292be6d83caab
Dan <dan.taylor1493@gmail.com>
2025-01-06 07:36:25 +0100

    Import server to server.js and change app.listen to server

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js"
-import { app } from "./socket/socket.js";
+import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 
@@ -22,7 +22,7 @@ app.use("/api/users", userRoutes);
 //   res.send("Hello World!!");
 // });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Server running on port ${PORT}`);
 });
