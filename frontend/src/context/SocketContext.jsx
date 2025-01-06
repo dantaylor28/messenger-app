6c150115906bfb091a4f7c8a19d6416bb8a5d871
Dan <dan.taylor1493@gmail.com>
2025-01-06 08:09:51 +0100

    Create useEffect in socketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -1,8 +1,30 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import { useAuthContext } from "./AuthContext";
 
 export const SocketContext = createContext();
 
 export const SocketContextProvider = ({ children }) => {
-    
-  return <SocketContext.Provider value={{}}>{children}</SocketContext.Provider>;
+  const [socket, setSocket] = useState(null);
+  const [onlineUsers, setOnlineUsers] = useState([]);
+  const { authenticatedUser } = useAuthContext();
+
+  useEffect(() => {
+    if (authenticatedUser) {
+      const socket = io("http://localhost:5173");
+
+      setSocket(socket);
+
+      return () => socket.close();
+    } else {
+      if (socket) {
+        socket.close();
+        setSocket(null);
+      }
+    }
+  }, []);
+  return (
+    <SocketContext.Provider value={{ socket, onlineUsers }}>
+      {children}
+    </SocketContext.Provider>
+  );
 };
