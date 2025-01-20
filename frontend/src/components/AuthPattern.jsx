7e4e68e89b7756bd41dc57bda8b1889d24e623fc
Dan <dan.taylor1493@gmail.com>
2025-01-20 21:57:02 +0100

    Add unstyled, skeleton auth pattern

--- frontend/src/components/AuthPattern.jsx
+++ frontend/src/components/AuthPattern.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
-const AuthPattern = () => {
+const AuthImagePattern = () => {
   return (
-    <div>AuthPattern</div>
-  )
-}
+    <div className="hidden lg:flex items-center justify-center bg-red-200">
+      <div className="max-w-md text-center">
+        <div className="grid grid-cols-3 gap-10 mb-8">
+          {[...Array(9)].map((_, i) => (
+            <div
+              key={i}
+              className={`aspect-square rounded-3xl bg-red-500 h-32 w-32 ${
+                i % 2 === 0 ? "animate-pulse" : ""
+              }`}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default AuthPattern
+export default AuthImagePattern;
