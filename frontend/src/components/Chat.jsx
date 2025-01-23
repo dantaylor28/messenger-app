35745a7f6d2d3cc5702f375b3e549eda9919842d
Dan <dan.taylor1493@gmail.com>
2025-01-23 08:38:20 +0100

    Define handleClick to check whether onClick in Chat is defined before calling it to prevent errors

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -8,16 +8,21 @@ const Chat = ({ chat, onClick, lastIndex }) => {
   const isSelected = selectedChat?._id === chat._id;
   const { onlineUsers } = useSocketContext();
   const isOnline = onlineUsers.includes(chat._id);
+
+  const handleClick = () => {
+    // Check if onClick is defined and then call it
+    if (onClick) {
+      onClick();
+    }
+    setSelectedChat(chat);
+  };
   return (
     <>
       <div
         className={`flex gap-4 py-2 px-2 rounded-sm items-center cursor-pointer hover:bg-cyan-700/60 md:hover:bg-gray-200/60 ${
           isSelected ? "bg-cyan-700/60 md:bg-gray-200/60" : ""
         }`}
-        onClick={() => {
-          setSelectedChat(chat);
-          onClick();
-        }}
+        onClick={handleClick}
       >
         <div className="py-1.5">
           <div className="w-12 md:w-14 rounded-full border border-black/10 relative">
