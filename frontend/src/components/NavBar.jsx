cf88f287dd020b82f6d4bb539c992075f9f09541
Dan <dan.taylor1493@gmail.com>
2025-01-25 16:16:39 +0100

    Add basic styling to NavBar.jsx

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className='flex items-center bg-amber-600/60 h-16'>
+      <div className='ml-10 text-xl font-serif'>ChatApp</div>
+    </div>
   )
 }
 
