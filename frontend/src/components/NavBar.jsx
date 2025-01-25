2b4e98a611c26bbbb5da364d35aea1f750c01041
Dan <dan.taylor1493@gmail.com>
2025-01-25 16:47:14 +0100

    Add basic styling to settings btn

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -9,8 +9,8 @@ const NavBar = () => {
       <div className="size-12 rounded-xl flex items-center justify-center bg-white/70 ml-8">
         <MessagesSquare className="size-6 text-amber-800" />
       </div>
-      <div className="ml-auto mr-8">
-        <Settings />
+      <div className="flex items-center ml-auto mr-8 cursor-pointer bg-white/70 p-1.5 rounded-lg">
+        <Settings className="size-6 text-amber-800" />
       </div>
     </div>
   );
