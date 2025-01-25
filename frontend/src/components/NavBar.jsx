25e12868980f12f6ae6c5efe69d8b103715fccc4
Dan <dan.taylor1493@gmail.com>
2025-01-25 16:27:54 +0100

    Add logo to NavBar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import { MessagesSquare } from "lucide-react";
 
 const NavBar = () => {
   return (
-    <div className='flex items-center bg-amber-600/60 h-16'>
-      <div className='ml-10 text-xl font-serif'>ChatApp</div>
+    <div className="flex items-center bg-amber-600/60 h-16">
+      {/* Logo */}
+      <div className="size-12 rounded-xl flex items-center justify-center bg-white/70 ml-8">
+        <MessagesSquare className="size-6 text-amber-800" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
