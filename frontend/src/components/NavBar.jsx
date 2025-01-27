bbfdffbdd055e66ba808f24c6548390bd0dc442e
Dan <dan.taylor1493@gmail.com>
2025-01-27 18:20:23 +0100

    Add styled profile btn to NavBar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { MessagesSquare, Settings } from "lucide-react";
-
+import { MessagesSquare, Settings, User } from "lucide-react";
 
 const NavBar = () => {
   return (
@@ -9,9 +8,15 @@ const NavBar = () => {
       <div className="size-12 rounded-xl flex items-center justify-center bg-white/70 ml-8">
         <MessagesSquare className="size-6 text-amber-800" />
       </div>
-      <div className="flex items-center ml-auto mr-8 cursor-pointer bg-amber-600/40 p-2 rounded-full hover:bg-amber-600/55 transition gap-1">
-        <Settings className="size-5 text-white" />
-        <p className="text-xs font-semibold text-white">Settings</p>
+      <div className="flex ml-auto">
+        <div className="flex items-center mr-4 cursor-pointer bg-amber-600/40 p-2 rounded-full hover:bg-amber-600/55 transition gap-1">
+          <User className="size-5 text-white" />
+          <p className="text-xs font-semibold text-white">Profile</p>
+        </div>
+        <div className="flex items-center mr-8 cursor-pointer bg-amber-600/40 p-2 rounded-full hover:bg-amber-600/55 transition gap-1">
+          <Settings className="size-5 text-white" />
+          <p className="text-xs font-semibold text-white">Settings</p>
+        </div>
       </div>
     </header>
   );
