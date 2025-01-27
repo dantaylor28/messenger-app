2f35da3f7ce7e461a092c84141e1d0696d3bb4c0
Dan <dan.taylor1493@gmail.com>
2025-01-27 18:11:09 +0100

    Customise settings btn in NavBar.jsx

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -9,8 +9,9 @@ const NavBar = () => {
       <div className="size-12 rounded-xl flex items-center justify-center bg-white/70 ml-8">
         <MessagesSquare className="size-6 text-amber-800" />
       </div>
-      <div className="flex items-center ml-auto mr-8 cursor-pointer bg-amber-600/40 p-2 rounded-full hover:bg-amber-600/55 transition">
-        <Settings className="size-6 text-white" />
+      <div className="flex items-center ml-auto mr-8 cursor-pointer bg-amber-600/40 p-2 rounded-full hover:bg-amber-600/55 transition gap-1">
+        <Settings className="size-5 text-white" />
+        <p className="text-xs font-semibold text-white">Settings</p>
       </div>
     </header>
   );
