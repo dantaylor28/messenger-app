83ba2fe360732c16b482f290dac1eeb2fcbade57
Dan <dan.taylor1493@gmail.com>
2025-01-27 18:07:09 +0100

    Replace navbar opening div with a header tag

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import { MessagesSquare, Settings } from "lucide-react";
 
 const NavBar = () => {
   return (
-    <div className="flex items-center bg-amber-600/60 h-16">
+    <header className="flex items-center bg-amber-600/60 h-16">
       {/* Logo */}
       <div className="size-12 rounded-xl flex items-center justify-center bg-white/70 ml-8">
         <MessagesSquare className="size-6 text-amber-800" />
@@ -12,7 +12,7 @@ const NavBar = () => {
       <div className="flex items-center ml-auto mr-8 cursor-pointer bg-amber-600/40 p-2 rounded-full hover:bg-amber-600/55 transition">
         <Settings className="size-6 text-white" />
       </div>
-    </div>
+    </header>
   );
 };
 
