8f283719d5cdbfe0c067c7da8c28923748c13abd
Dan <dan.taylor1493@gmail.com>
2025-01-27 18:37:29 +0100

    Replace divs with Links and dynamically display depending on Authenticated status

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { MessagesSquare, Settings, User } from "lucide-react";
+import { useAuthContext } from "../context/AuthContext";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
+  const { authenticatedUser } = useAuthContext();
   return (
     <header className="flex items-center bg-amber-600/60 h-16">
       {/* Logo */}
@@ -9,14 +12,18 @@ const NavBar = () => {
         <MessagesSquare className="size-6 text-amber-800" />
       </div>
       <div className="flex ml-auto">
-        <div className="flex items-center mr-4 cursor-pointer bg-amber-600/40 p-2 rounded-full hover:bg-amber-600/55 transition gap-1">
-          <User className="size-5 text-white" />
-          <p className="text-xs font-semibold text-white">Profile</p>
-        </div>
-        <div className="flex items-center mr-8 cursor-pointer bg-amber-600/40 p-2 rounded-full hover:bg-amber-600/55 transition gap-1">
+        {authenticatedUser && (
+          <>
+            <Link className="flex items-center mr-4 cursor-pointer bg-amber-600/40 p-2 rounded-full hover:bg-amber-600/55 transition gap-1">
+              <User className="size-5 text-white" />
+              <p className="text-xs font-semibold text-white">Profile</p>
+            </Link>
+          </>
+        )}
+        <Link className="flex items-center mr-8 cursor-pointer bg-amber-600/40 p-2 rounded-full hover:bg-amber-600/55 transition gap-1">
           <Settings className="size-5 text-white" />
           <p className="text-xs font-semibold text-white">Settings</p>
-        </div>
+        </Link>
       </div>
     </header>
   );
