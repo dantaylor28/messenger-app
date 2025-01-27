d41d215218ed4778acce0a7b9609314ac54a2cfe
Dan <dan.taylor1493@gmail.com>
2025-01-27 18:55:47 +0100

    Add Link tag to Logo and add navigation to it and the profile Link

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -8,13 +8,13 @@ const NavBar = () => {
   return (
     <header className="flex items-center bg-amber-600/60 h-16">
       {/* Logo */}
-      <div className="size-12 rounded-xl flex items-center justify-center bg-white/70 ml-8">
+      <Link className="size-12 rounded-xl flex items-center justify-center bg-white/70 ml-8" to="/">
         <MessagesSquare className="size-6 text-amber-800" />
-      </div>
+      </Link>
       <div className="flex ml-auto">
         {authenticatedUser && (
           <>
-            <Link className="flex items-center mr-4 cursor-pointer bg-amber-600/40 p-2 rounded-full hover:bg-amber-600/55 transition gap-1">
+            <Link className="flex items-center mr-4 cursor-pointer bg-amber-600/40 p-2 rounded-full hover:bg-amber-600/55 transition gap-1" to="/profile">
               <User className="size-5 text-white" />
               <p className="text-xs font-semibold text-white">Profile</p>
             </Link>
