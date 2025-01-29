1f81e3afcc55a910acfe84635422376ea1b9b518
Dan <dan.taylor1493@gmail.com>
2025-01-29 06:46:14 +0100

    Add username input to profile page

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import useUpdateProfile from "../hooks/useUpdateProfile";
 import { useAuthContext } from "../context/AuthContext";
-import { User } from "lucide-react";
+import { User, UserCheck } from "lucide-react";
 
 const Profile = () => {
   // const { sendingData, setSendingData, updateProfile } = useUpdateProfile();
@@ -19,18 +19,37 @@ const Profile = () => {
       </div>
       <div className="flex items-center justify-center">
         <div className="w-[30%]">
-          <div className="flex items-center gap-0.5 mt-4">
-            <User className="size-4" />
-            <label htmlFor="full-name">
-              <span className="text-xs font-medium">Full Name</span>
-            </label>
-          </div>
+          {/* Full name input */}
           <div>
-            <input
-              type="text"
-              value={authenticatedUser.fullName}
-              className="w-full bg-slate-40 border border-black/40 pl-3 rounded-[4px] h-10 focus:outline-none focus:border-black/80"
-            />
+            <div className="flex items-center gap-0.5 mt-4">
+              <User className="size-4" />
+              <label htmlFor="full-name">
+                <span className="text-xs font-medium">Full Name</span>
+              </label>
+            </div>
+            <div>
+              <input
+                type="text"
+                value={authenticatedUser.fullName}
+                className="w-full bg-slate-40 border border-black/40 pl-3 rounded-[4px] h-10 focus:outline-none focus:border-black/80"
+              />
+            </div>
+          </div>
+          {/* Username input */}
+          <div>
+            <div className="flex items-center gap-0.5 mt-3">
+              <UserCheck className="size-4" />
+              <label htmlFor="username">
+                <span className="text-xs font-medium">Username</span>
+              </label>
+            </div>
+            <div>
+              <input
+                type="text"
+                value={authenticatedUser.username}
+                className="w-full bg-slate-40 border border-black/40 pl-3 rounded-[4px] h-10 focus:outline-none focus:border-black/80"
+              />
+            </div>
           </div>
         </div>
       </div>
