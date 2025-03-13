dd2a6265b7a9fc76189dc2470e7d1fe51267053e
Dan <dan.taylor1493@gmail.com>
2025-03-13 09:06:16 +0100

    Update template shown when no message history exists

--- frontend/src/components/Messages.jsx
+++ frontend/src/components/Messages.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import Message from "./Message";
 import useGetMessages from "../hooks/useGetMessages";
-import { PiChatDotsThin } from "react-icons/pi";
+import { Send } from 'lucide-react';
 import ChatSkeleton from "./ChatSkeleton";
 
 const Messages = () => {
@@ -17,16 +17,20 @@ const Messages = () => {
   return (
     <div className="flex-1 overflow-auto px-4 w-[100vw] md:w-full">
       {!sendingData && messages.length === 0 && (
-        <div className="flex flex-col items-center w-full mt-20">
-          <div>
-            <PiChatDotsThin className="h-32 w-32 items-center justify-center text-cyan-800" />
+        <div className="flex flex-col items-center w-full mt-36">
+          <div className="size-20 rounded-xl flex items-center justify-center bg-amber-400/30 mb-4 group hover:bg-amber-400/35 animate-bounce">
+            <Send className="size-10 text-amber-800 group-hover:text-amber-900" />
           </div>
-          <p className="text-center mt-3 text-xl font-extralight tracking-wide">
-            Send a message to start the chat!
+          <p className="text-center mt-3 capitalize font-medium text-2xl tracking-wide">
+            no chat history!
+          </p>
+          <p className="mt-2 text-md font-light">
+            Send a message to start the chat
           </p>
         </div>
       )}
-      {sendingData && [...Array(4)].map((_, index) => <ChatSkeleton key={index} />)}
+      {sendingData &&
+        [...Array(4)].map((_, index) => <ChatSkeleton key={index} />)}
 
       {!sendingData &&
         messages.length > 0 &&
