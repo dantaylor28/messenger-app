45342d664d8e55fae3c5838652bb5065d3a2d471
Dan <dan.taylor1493@gmail.com>
2025-04-02 08:28:04 +0200

    Add styled sidebar heading

--- frontend/src/components/Chats.jsx
+++ frontend/src/components/Chats.jsx
@@ -3,12 +3,17 @@ import Chat from "./Chat";
 import useGetChats from "../hooks/useGetChats";
 import useListenForMessages from "../hooks/useListenForMessages";
 import SidebarSkeleton from "./SidebarSkeleton"
+import { Users } from 'lucide-react';
 
 const Chats = ({ onChatClick }) => {
   const { sendingData, chats } = useGetChats();
   useListenForMessages();
   return (
-    <div className="flex flex-col overflow-auto mt-8 w-full">
+    <div className="flex flex-col overflow-auto mt-8 w-full pr-12">
+      <div className="flex items-center gap-2 mb-5 px-5 py-3 border-b">
+        <Users className="size-6 text-amber-700" />
+        <span className="font-medium opacity-75">Contacts</span>
+      </div>
       {chats.map((chat, idx) => (
         <Chat
           key={chat._id}
