ada18aec8a68ddc03629d4f689f8fe996cf47784
Dan <dan.taylor1493@gmail.com>
2025-04-03 08:12:43 +0200

    opacity and padding changes to mobile sidebar

--- frontend/src/components/Chats.jsx
+++ frontend/src/components/Chats.jsx
@@ -9,10 +9,10 @@ const Chats = ({ onChatClick }) => {
   const { sendingData, chats } = useGetChats();
   useListenForMessages();
   return (
-    <div className="flex flex-col overflow-auto mt-8 w-full md:pr-12">
+    <div className="flex flex-col overflow-auto mt-8 w-full md:pr-6 lg:pr-10 xl:pr-12">
       <div className="flex items-center gap-2 mb-5 px-5 py-3 border-b border-white/50 md:border-black/30">
-        <Users className="size-6 text-amber-700" />
-        <span className="font-medium opacity-75">Contacts</span>
+        <Users className="size-6 text-white/90 md:text-amber-700" />
+        <span className="font-medium md:opacity-75">Contacts</span>
       </div>
       {chats.map((chat, idx) => (
         <Chat
