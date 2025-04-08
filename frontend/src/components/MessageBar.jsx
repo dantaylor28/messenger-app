4c5374f199414b65b0ab8f056accfb056d157a13
Dan <dan.taylor1493@gmail.com>
2025-04-08 22:16:24 +0200

    Replace icon in messageBar with styled luicide icon

--- frontend/src/components/MessageBar.jsx
+++ frontend/src/components/MessageBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import useSendMessage from "../hooks/useSendMessage";
-import { IoSend } from "react-icons/io5";
+import { SendHorizonal, SendHorizontal } from 'lucide-react';
 
 const MessageBar = () => {
   const [message, setMessage] = useState("");
@@ -17,7 +17,7 @@ const MessageBar = () => {
         <input
           type="text"
           placeholder="Enter message"
-          className="border-y border-r border-black/20 focus:outline-amber-500/50 w-full h-12 rounded-sm text-md pl-2 shadow-sm cursor-text pr-12"
+          className="border-y border-r border-black/20 focus:outline-black/25 w-full h-12 rounded-sm text-md pl-2 shadow-sm cursor-text pr-12"
           value={message}
           onChange={(e) => setMessage(e.target.value)}
         />
@@ -28,7 +28,7 @@ const MessageBar = () => {
           {sendingData ? (
             <div className="animate-spin h-5 w-5 rounded-full border-b-2 border-cyan-800"></div>
           ) : (
-            <IoSend className="h-[22px] w-[22px] text-cyan-600 hover:text-cyan-700 hover:translate-x-0.5 transition hover:cursor-pointer" />
+            <SendHorizonal className="h-[22px] w-[22px] text-amber-600 hover:text-amber-700 hover:translate-x-0.5 transition hover:cursor-pointer" />
           )}
         </button>
       </div>
