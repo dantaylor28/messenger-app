c56b9aa0f4e69124c38824d3f0958d20822c7e8a
Dan <dan.taylor1493@gmail.com>
2025-04-10 07:35:49 +0200

    Add basic image placeholder in messageBar component

--- frontend/src/components/MessageBar.jsx
+++ frontend/src/components/MessageBar.jsx
@@ -8,9 +8,9 @@ const MessageBar = () => {
   const fileInputRef = useRef(null);
   const { sendingData, sendMessage } = useSendMessage();
 
-  const handleChangeImage = (e) => {}
+  const handleChangeImage = (e) => {};
 
-  const removeImage = () => {}
+  const removeImage = () => {};
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -19,27 +19,47 @@ const MessageBar = () => {
     setMessage("");
   };
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="relative">
-        <input
-          type="text"
-          placeholder="Enter message"
-          className="border-y border-r border-black/20 focus:outline-black/25 w-full h-12 rounded-sm text-md pl-2 shadow-sm cursor-text pr-12"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-        />
-        <button
-          type="submit"
-          className="absolute inset-y-0 end-0 items-center pe-3"
-        >
-          {sendingData ? (
-            <div className="animate-spin h-5 w-5 rounded-full border-b-2 border-cyan-800"></div>
-          ) : (
-            <SendHorizonal className="h-[22px] w-[22px] text-amber-600 hover:text-amber-700 hover:translate-x-0.5 transition hover:cursor-pointer" />
-          )}
-        </button>
-      </div>
-    </form>
+    <div>
+      {imagePreview && (
+        <div className="flex">
+          <div className="relative">
+            <img
+              src={imagePreview}
+              alt="image preview"
+              className="w-12 h-12 object-cover"
+            />
+            <button
+              onClick={removeImage}
+              type="button"
+              className="absolute -top-1.5 -right-1-5 w-5 h-5"
+            >
+              X
+            </button>
+          </div>
+        </div>
+      )}
+      <form onSubmit={handleSubmit}>
+        <div className="relative">
+          <input
+            type="text"
+            placeholder="Enter message"
+            className="border-y border-r border-black/20 focus:outline-black/25 w-full h-12 rounded-sm text-md pl-2 shadow-sm cursor-text pr-12"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="absolute inset-y-0 end-0 items-center pe-3"
+          >
+            {sendingData ? (
+              <div className="animate-spin h-5 w-5 rounded-full border-b-2 border-cyan-800"></div>
+            ) : (
+              <SendHorizonal className="h-[22px] w-[22px] text-amber-600 hover:text-amber-700 hover:translate-x-0.5 transition hover:cursor-pointer" />
+            )}
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
 
