51e16511c76351a0c6e9ea43816e955b753bb180
Dan <dan.taylor1493@gmail.com>
2025-04-18 07:12:20 +0200

    Add text and start to style sidebar checkbox

--- frontend/src/components/Chats.jsx
+++ frontend/src/components/Chats.jsx
@@ -16,12 +16,20 @@ const Chats = ({ onChatClick }) => {
   useListenForMessages();
   return (
     <div className="flex flex-col overflow-auto mt-8 w-full md:pr-6 lg:pr-10 xl:pr-12">
-      <div className="flex items-center gap-2 mb-5 px-5 py-3 border-b border-white/50 md:border-black/30">
+      <div className="flex items-center gap-2 mb-3 px-5 py-3 border-b border-white/50 md:border-black/30">
         <Users className="size-6 text-white/90 md:text-amber-700" />
         <span className="font-medium md:opacity-75">Contacts</span>
-        <div>
-          <input type="checkbox" checked={showActiveUsers} onChange={(e) => setShowActiveUsers(e.target.checked)} />
-        </div>
+      </div>
+      <div className="flex items-center mb-5">
+        <label className="flex items-center gap-2 cursor-pointer">
+          <input
+            type="checkbox"
+            checked={showActiveUsers}
+            onChange={(e) => setShowActiveUsers(e.target.checked)}
+            className="cursor-pointer"
+          />
+          <span>Show active users</span>
+        </label>
       </div>
       {filteredUsers.map((chat, idx) => (
         <Chat
