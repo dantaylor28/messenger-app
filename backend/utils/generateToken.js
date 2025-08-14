5939d707c8ed82aa563d0af38a2a2dcabb2511bb
Dan <dan.taylor1493@gmail.com>
2025-08-14 08:01:08 +0200

    Fix typo in generateToken.js file

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -9,7 +9,7 @@ const generateToken = (userId, res) => {
     maxAge: 14 * 24 * 60 * 60 * 1000, // 14d in milliseconds format
     httpOnly: true, // Prevent XXS/cross-site scripting attacks
     sameSite: "strict", // Prevent CSRF/cross-site request forgery attacks
-    secure: process.env.NODE.ENV !== "development",
+    secure: process.env.NODE_ENV !== "development",
   });
 };
 
